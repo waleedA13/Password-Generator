ebe536d6b75e1ea262c07058259b4356970d1333
Waleed <waleed@Waleeds-MBP.attlocal.net>
2023-01-09 00:42:47 -0800

    Removed Easy Mode

--- main.py
+++ main.py
@@ -9,21 +9,7 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level
-#password = ""
-#for letter in range(1, nr_letters + 1):
-  #password += random.choice(letters)
-  
-#for number in range(1, nr_numbers + 1):
-  #password += random.choice(numbers)
 
-
-#for symbol in range(1, nr_symbols + 1):
-  #password += random.choice(symbols)
-
-
-#print(password)
-#Hard Level
 password_list = []
 random_password = ""
 for letter in range(1, nr_letters + 1):
